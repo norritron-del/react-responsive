fb93bdd9f324f09b63282981a463ad74d6e63502
norritron-del <norritron@gmail.com>
2026-06-01 09:10:30 +1200

    Update FullHeading.tsx

--- components/FullHeading.tsx
+++ components/FullHeading.tsx
@@ -2,7 +2,7 @@ export default function FullHeading() {
   return (
     <section className="md:col-span-6 mb-16">
       <h1 className="text-4xl md:text-6xl lg:text-8xl font-black tracking-tight prose max-w-3xl">
-        This chonky heading is taking up space at the top of the page
+        The biggest thing in the world is a giant tortoise
       </h1>
     </section>
   );
